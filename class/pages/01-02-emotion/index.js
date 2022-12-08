a76a683351c1ad3a84ae1bee8ace671e7312a551
25rae25 <whdudfo0124@gmail.com>
2022-12-08 17:21:28 +0900

    Day01

---
+++ class/pages/01-02-emotion/index.js
@@ -0,0 +1,13 @@
+import { EmailInput, Wrapper } from '../../styles/emotion'
+
+export default function MyPage() {
+
+	// 자바스크립트 작성
+
+	return (
+		<Wrapper>
+			테스트중
+			<EmailInput type="text" />
+		</Wrapper>
+	)
+}
